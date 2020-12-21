682f729094a06776f8b116f3e7f3b159786f3478
Shayne Vaykovich <VAYKO@Shaynes-MacBook-Pro.local>
2020-12-21 09:24:10 -0500

    saved

--- app.js
+++ app.js
@@ -1,37 +1,62 @@
-const DOMAIN = 'https://statsapi.web.nhl.com/api/v1/teams';
-const API_KEY = `d2e85759`
-const BASE_URL = `${DOMAIN}?apikey=${API_KEY}&`
-
-const getTeam = async () => {
+const getData = async () => {
   try {
-
-
-  
     const getInfo = document.querySelector("input").value
-    // console.log(getInfo)
-    const response = await axios.get(`${BASE_URL}s=${getInfo}`)
-    console.log(response.data.Search)
-    renderList(response.data.Search)
-    return response
-  } catch (error) {
-    // console.log()
+    const URL = `https://cors-anywhere.herokuapp.com/https://records.nhl.com/site/api/record-detail`
+    const response = await axios.get(URL)
+    console.log(response.data)
+    const axiosData = response.data
+    console.log(axiosData)
+  }
+  catch {
+    (error)
+    console.log(error)
   }
 }
-const searchButton = document.querySelector("#search")
-searchButton.addEventListener("click", getTeam)
+    getData()
 
-
+    const searchButton = document.querySelector("#search")
+searchButton.addEventListener("click", getData)
+    
 async function renderList(teams) {
   teams.forEach((team) => {
-    let teamName = document.createElement ('h2')
-    teamName.innerText = team.Name
+    let teamName = document.createElement('h2')
+    teamName.innerText = team.Title
     document.querySelector('.team-list').append(teamName)
-    let img = document.createElement('img')
-    img.src = team.Poster
-    document.querySelector('.team-list').append(img)
+    
   })
-  
-  
-  // console.log(renderList)
 }
-// [...renderList].forEach(renderList => {
+renderList()
+
+
+
+
+
+// const getData = async () => {
+//   try {
+//     const headers = {
+//       headers: {
+//         'Access-Control-Allow-Origin': '*',
+//       },
+//     }
+//     const URL = 'http://www.nokeynoshade.party/api/queens/all'
+//     const response = await axios.get(URL, headers)
+//     console.log(response)
+//     const axiosData = response.data
+//     console.log(axiosData)
+//     queenInfo(axiosData)
+//   } catch {}
+// }
+// getData()
+// const queenInfo = (queens) => {
+//   queens.forEach((queen) => {
+//     const queenName = document.createElement('h3')
+//     queenName.innerHTML = queen.name
+//     const queenQuote = document.createElement('h5')
+//     queenQuote.innerHTML = `Quote: "${queen.quote}"`
+//     const queenImg = document.createElement('img')
+//     queenImg.setAttribute('src', queen.image_url)
+//     document.body.append(queenImg)
+//     document.body.append(queenName)
+//     document.body.append(queenQuote)
+//   })
+// }
