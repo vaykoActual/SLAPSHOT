1b7d117ea39b059af8529d93c492b756e6356e69
Shayne Vaykovich <VAYKO@Shaynes-MacBook-Pro.local>
2020-12-22 14:53:11 -0500

    render

--- app.js
+++ app.js
@@ -1,33 +1,94 @@
+///###SLAPSHOT###
+
+
+const DOMAIN = `https://statsapi.web.nhl.com/api/v1/teams/`
+const teamSection = document.querySelector(".team-list")
+// const API_KEY = `d2e85759`
+// const BASE_URL = `${DOMAIN}?apikey=${API_KEY}&`
+
+
 const getData = async () => {
   try {
     // const getInfo = document.querySelector("input").value
-    const URL = `https://cors-anywhere.herokuapp.com/https://records.nhl.com/site/api/record-detail`
-    const response = await axios.get(URL)
+    // const URL = `https://cors-anywhere.herokuapp.com/https://statsapi.web.nhl.com/api/v1/teams/`
+    const response = await axios.get(`${DOMAIN}`)
     // console.log(response.data)
-    const axiosData = response.data
-    console.log(axiosData)
+    // const axiosData = (response.forE)
+    // console.log(axiosData)
+    //renderList(response.data.search)
+    renderList(response.data.teams)
   }
-  catch {
-    (error)
-    console.log(error)
+  catch (error) {
+    //console.log(error)
   }
 }
-    getData()
+getData()
 
-    const searchButton = document.querySelector("#search")
-searchButton.addEventListener("click", getData)
+
+// const searchButton = document.querySelector("#search")
+// searchButton.addEventListener("click", getData)  
+
+function renderList(teams) {
+  teams.forEach((team) => {
+    let teamDiv = document.createElement('div')
+    teamDiv.innerHTML = `
+    <h2>${team.name}</h2>
+    `
+    teamSection.append(teamDiv)
+
+
+      // let teamName = document.createElement('h2')
+      // teamName.innerText = teams.Name
+      // document.querySelector('.team-list').append(teamName)
+      
+    })
+  }
+  
+
+
+        
+
+
+
+// const DOMAIN = 'https://cors-anywhere.herokuapp.com/https://records.nhl.com/site/api/record-detail/'
+// const API_KEY = `d2e85759`
+// const BASE_URL = `${DOMAIN}?apikey=${API_KEY}&`
+
+
+// const getData = async () => {
+//   try {
+//     // const headers = {
+//     //   headers: {
+//     //     'Access-Control-Allow-Origin': '*',
+//     //   },
+//     // }
+//     const getInfo = document.querySelector("input").value
+//     const response = await axios.get(`${BASE_URL}s=${getInfo}`)
+//     console.log(response.data.search)
+//     return response
+//     // const axiosData = response.data
+//     // console.log(axiosData)
+//   } catch (error) {
+//     //console.log(error)
+//   }
+// }
+//     getData()
+
+//     const searchButton = document.querySelector("#search")
+// searchButton.addEventListener("click", getData)
     
-async function renderList(data) {
-  data.forEach((team) => {
-    let teamName = document.createElement('h2')
-    teamName.innerText = team.Title
-    document.querySelector('.team-list').append(teamName)
+// async function renderList(data) {
+//   data.forEach((team) => {
+//     console.log(data.info)
+//     let teamName = document.createElement('h2')
+//     teamName.innerText = team.Title
+//     document.querySelector('.team-list').append(teamName)
     
-  })
-}
-renderList()
+//   })
+// }
+// renderList()
 
 
-
+// https://cors-anywhere.herokuapp.com/
 
 
--- test.js
+++ test.js
@@ -5,25 +5,32 @@
 //         'Access-Control-Allow-Origin': '*',
 //       },
 //     }
-//     const URL = 'http://www.nokeynoshade.party/api/queens/all'
+//     const URL = 'https://records.nhl.com/site/api/record-detail/'
 //     const response = await axios.get(URL, headers)
-//     console.log(response)
-//     const axiosData = response.data
-//     console.log(axiosData)
-//     queenInfo(axiosData)
+//     console.log(response.data)
+//     // const axiosData = response.data
+//     // console.log(axiosData)
+  
 //   } catch {}
 // }
 // getData()
-// const queenInfo = (queens) => {
-//   queens.forEach((queen) => {
-//     const queenName = document.createElement('h3')
-//     queenName.innerHTML = queen.name
-//     const queenQuote = document.createElement('h5')
-//     queenQuote.innerHTML = `Quote: "${queen.quote}"`
-//     const queenImg = document.createElement('img')
-//     queenImg.setAttribute('src', queen.image_url)
-//     document.body.append(queenImg)
-//     document.body.append(queenName)
-//     document.body.append(queenQuote)
-//   })
-// }
+
+
+//     const searchButton = document.querySelector("#search")
+// searchButton.addEventListener("click", getData)
+    
+
+
+// // const queenInfo = (queens) => {
+// //   queens.forEach((queen) => {
+// //     const queenName = document.createElement('h3')
+// //     queenName.innerHTML = queen.name
+// //     const queenQuote = document.createElement('h5')
+// //     queenQuote.innerHTML = `Quote: "${queen.quote}"`
+// //     const queenImg = document.createElement('img')
+// //     queenImg.setAttribute('src', queen.image_url)
+// //     document.body.append(queenImg)
+// //     document.body.append(queenName)
+// //     document.body.append(queenQuote)
+// //   })
+// // }
