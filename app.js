c65eff3ef92011689e6aafe86dde31860a9cd4e0
Shayne Vaykovich <VAYKO@Shaynes-MacBook-Pro.local>
2020-12-22 18:04:49 -0500

    saveNoSearch

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
 ///###SLAPSHOT###
 
 
-const DOMAIN = `https://statsapi.web.nhl.com/api/v1/teams/`
-const teamSection = document.querySelector(".team-list")
+
 // const API_KEY = `d2e85759`
 // const BASE_URL = `${DOMAIN}?apikey=${API_KEY}&`
+
+const DOMAIN = `http://statsapi.web.nhl.com/api/v1/teams/`
+const teamSection = document.querySelector(".team-list")
 
 
 const getData = async () => {
   try {
     const getInfo = document.querySelector("input").value
-    // const URL = `https://cors-anywhere.herokuapp.com/https://statsapi.web.nhl.com/api/v1/teams/`
+    // const URL = `https://cors-anywhere.herokuapp.com/https://statsapi.web.nhl.com/api/v1/${getInfo}/`
     const response = await axios.get(`${DOMAIN}`)
-    // console.log(response.data)
+    console.log(response.data)
     // const axiosData = (response.forE)
-    // console.log(axiosData)
+    //console.log(axiosData)
     //renderList(response.data.search)
-    renderList(response.data.teams)
+    renderList(response.data.Search)
   }
   catch (error) {
     //console.log(error)
@@ -32,7 +34,7 @@ function renderList(teams) {
   teams.forEach((team) => {
     let teamDiv = document.createElement('div')
     teamDiv.innerHTML = `
-    <h2>${team.name}</h2>
+    <h2>${teams.Search}</h2>
     `
     teamSection.append(teamDiv)
 
@@ -92,3 +94,26 @@ function renderList(teams) {
 // https://cors-anywhere.herokuapp.com/
 
 
+// conferences => https://statsapi.web.nhl.com/api/v1/conferences
+// dates => https://statsapi.web.nhl.com/api/v1/schedule?season=20172018
+// divisions => https://statsapi.web.nhl.com/api/v1/divisions
+// franchises => https://statsapi.web.nhl.com/api/v1/franchises
+// games => https://statsapi.web.nhl.com/api/v1/schedule?season=20172018
+// game_status => https://statsapi.web.nhl.com/api/v1/gameStatus
+// game_types => https://statsapi.web.nhl.com/api/v1/gameTypes
+// game_results => https://statsapi.web.nhl.com/api/v1/playTypes
+// game_events, game_events_players => https://statsapi.web.nhl.com/api/v1/game/2017020395/feed/live
+// game_shifts => https://api.nhle.com/stats/rest/en/shiftcharts?cayenneExp=gameId=2019030212
+// player => https://statsapi.web.nhl.com/api/v1/people/8471214
+// player_regular_season_stats - https://statsapi.web.nhl.com/api/v1/people/8471214?expand=person.stats&stats=yearByYear&expand=stats.team&site=en_nhl
+// player_playoffs_stats - https://statsapi.web.nhl.com/api/v1/people/8471214?expand=person.stats&stats=yearByYearPlayoffs&expand=stats.team&site=en_nhl
+// player_game_logs - https://statsapi.web.nhl.com/api/v1/people/8471214?expand=person.stats&stats=gameLog&expand=stats.team&site=en_nhl&season=20182019
+// play_types => https://statsapi.web.nhl.com/api/v1/playTypes
+// positions - https://statsapi.web.nhl.com/api/v1/positions
+// schedules => https://statsapi.web.nhl.com/api/v1/schedule?season=20172018
+// seasons => https://statsapi.web.nhl.com/api/v1/seasons
+// standings => https://statsapi.web.nhl.com/api/v1/standings/byLeague?season=20182019 , https://statsapi.web.nhl.com/api/v1/standings/byConference?season=20182019, https://statsapi.web.nhl.com/api/v1/standings/byDivision?season=20182019
+// standing_types => https://statsapi.web.nhl.com/api/v1/standingsTypes
+// team => https://statsapi.web.nhl.com/api/v1/teams/8
+// team_stats => http://statsapi.web.nhl.com/api/v1/teams/14?expand=team.stats&season=20192020
+// team_player(roster) => https://statsapi.web.nhl.com/api/v1/teams/8/roster
