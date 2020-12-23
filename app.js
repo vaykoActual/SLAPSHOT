ecebb8ca8578251c1bbf22bbcca5b3dc8eda9c12
Shayne Vaykovich <VAYKO@Shaynes-MacBook-Pro.local>
2020-12-23 09:37:52 -0500

    saveNoSearch

--- app.js
+++ app.js
@@ -42,21 +42,7 @@ function renderList(teams) {
 
 
 
-  // const teamInfo = (teams) => {
-    // //   queens.forEach((queen) => {
-    // //     const queenName = document.createElement('h3')
-    // //     queenName.innerHTML = queen.name
-    // //     const queenQuote = document.createElement('h5')
-    // //     queenQuote.innerHTML = `Quote: "${queen.quote}"`
-    // //     const queenImg = document.createElement('img')
-    // //     queenImg.setAttribute('src', queen.image_url)
-    // //     document.body.append(queenImg)
-    // //     document.body.append(queenName)
-    // //     document.body.append(queenQuote)
-    // //   })
-
-        
-
+//RESOURCES
 
 
 
