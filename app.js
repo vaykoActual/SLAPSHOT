89e3dcf42c53073e977f59e6f6773ebc9b70e5fd
Shayne Vaykovich <VAYKO@Shaynes-MacBook-Pro.local>
2020-12-23 09:07:33 -0500

    saveNoSearch

--- app.js
+++ app.js
@@ -15,13 +15,9 @@ const getData = async () => {
     // const URL = `https://cors-anywhere.herokuapp.com/https://statsapi.web.nhl.com/api/v1/${getInfo}/`
     const response = await axios.get(`${DOMAIN}`)
     console.log(response.data)
-    // const axiosData = (response.forE)
-    //console.log(axiosData)
-    //renderList(response.data.search)
-    renderList(response.data.Search)
+    renderList(response.data.searchButton)
   }
   catch (error) {
-    //console.log(error)
   }
 }
 getData()
@@ -34,61 +30,35 @@ function renderList(teams) {
   teams.forEach((team) => {
     let teamDiv = document.createElement('div')
     teamDiv.innerHTML = `
-    <h2>${teams.Search}</h2>
+    <h2>${team.name}</h2>
     `
-    teamSection.append(teamDiv)
-
-
-      // let teamName = document.createElement('h2')
-      // teamName.innerText = teams.Name
-      // document.querySelector('.team-list').append(teamName)
-      
+    const teamVenue = document.createElement('h3')
+    teamVenue.innerHTML = `${venue.name}`
+    teamSection.append(teamDiv) 
     })
   }
   
 
 
+
+
+  // const teamInfo = (teams) => {
+    // //   queens.forEach((queen) => {
+    // //     const queenName = document.createElement('h3')
+    // //     queenName.innerHTML = queen.name
+    // //     const queenQuote = document.createElement('h5')
+    // //     queenQuote.innerHTML = `Quote: "${queen.quote}"`
+    // //     const queenImg = document.createElement('img')
+    // //     queenImg.setAttribute('src', queen.image_url)
+    // //     document.body.append(queenImg)
+    // //     document.body.append(queenName)
+    // //     document.body.append(queenQuote)
+    // //   })
+
         
 
 
 
-// const DOMAIN = 'https://cors-anywhere.herokuapp.com/https://records.nhl.com/site/api/record-detail/'
-// const API_KEY = `d2e85759`
-// const BASE_URL = `${DOMAIN}?apikey=${API_KEY}&`
-
-
-// const getData = async () => {
-//   try {
-//     // const headers = {
-//     //   headers: {
-//     //     'Access-Control-Allow-Origin': '*',
-//     //   },
-//     // }
-//     const getInfo = document.querySelector("input").value
-//     const response = await axios.get(`${BASE_URL}s=${getInfo}`)
-//     console.log(response.data.search)
-//     return response
-//     // const axiosData = response.data
-//     // console.log(axiosData)
-//   } catch (error) {
-//     //console.log(error)
-//   }
-// }
-//     getData()
-
-//     const searchButton = document.querySelector("#search")
-// searchButton.addEventListener("click", getData)
-    
-// async function renderList(data) {
-//   data.forEach((team) => {
-//     console.log(data.info)
-//     let teamName = document.createElement('h2')
-//     teamName.innerText = team.Title
-//     document.querySelector('.team-list').append(teamName)
-    
-//   })
-// }
-// renderList()
 
 
 // https://cors-anywhere.herokuapp.com/
