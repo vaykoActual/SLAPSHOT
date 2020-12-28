c3348494a7101e4836eb7d4103c33a8b16465e47
Shayne Vaykovich <VAYKO@Shaynes-MacBook-Pro.local>
2020-12-28 09:10:02 -0500

    footer

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const teamSelectSection = document.querySelector(".team-select-section")
 //tryCatch
 const getData = async () => {
   try {
-    const getInfo = document.querySelector("input").value
+    const getInfo = document.querySelector("form").value
     // const URL = `https://cors-anywhere.herokuapp.com/https://statsapi.web.nhl.com/api/v1/${getInfo}/`
     const response = await axios.get(`${DOMAIN}`)
     console.log(response.data)
@@ -57,47 +57,11 @@ async function getTeamData(id) {
 function renderteamData(data) {
   let teamData = document.querySelector(".render-team-data")
   console.log(data.teams[0])
-  (teamData.textContent = data.teams[0].name)
-  
-}
- 
-  
+  teamData.innerHTML = `
+  <h1>${data.teams[0].name}</h1>
+  <h2>${data.teams[0].venue.name}</h2>
+  <h3>${data.teams[0].conference.name}</h3>
+  <h3>EST. ${data.teams[0].firstYearOfPlay}</h3>
+  `
+  }
 
-
-
-
-
-
-
-
-//RESOURCES
-
-
-
-
-// https://cors-anywhere.herokuapp.com/
-
-
-// conferences => https://statsapi.web.nhl.com/api/v1/conferences
-// dates => https://statsapi.web.nhl.com/api/v1/schedule?season=20172018
-// divisions => https://statsapi.web.nhl.com/api/v1/divisions
-// franchises => https://statsapi.web.nhl.com/api/v1/franchises
-// games => https://statsapi.web.nhl.com/api/v1/schedule?season=20172018
-// game_status => https://statsapi.web.nhl.com/api/v1/gameStatus
-// game_types => https://statsapi.web.nhl.com/api/v1/gameTypes
-// game_results => https://statsapi.web.nhl.com/api/v1/playTypes
-// game_events, game_events_players => https://statsapi.web.nhl.com/api/v1/game/2017020395/feed/live
-// game_shifts => https://api.nhle.com/stats/rest/en/shiftcharts?cayenneExp=gameId=2019030212
-// player => https://statsapi.web.nhl.com/api/v1/people/8471214
-// player_regular_season_stats - https://statsapi.web.nhl.com/api/v1/people/8471214?expand=person.stats&stats=yearByYear&expand=stats.team&site=en_nhl
-// player_playoffs_stats - https://statsapi.web.nhl.com/api/v1/people/8471214?expand=person.stats&stats=yearByYearPlayoffs&expand=stats.team&site=en_nhl
-// player_game_logs - https://statsapi.web.nhl.com/api/v1/people/8471214?expand=person.stats&stats=gameLog&expand=stats.team&site=en_nhl&season=20182019
-// play_types => https://statsapi.web.nhl.com/api/v1/playTypes
-// positions - https://statsapi.web.nhl.com/api/v1/positions
-// schedules => https://statsapi.web.nhl.com/api/v1/schedule?season=20172018
-// seasons => https://statsapi.web.nhl.com/api/v1/seasons
-// standings => https://statsapi.web.nhl.com/api/v1/standings/byLeague?season=20182019 , https://statsapi.web.nhl.com/api/v1/standings/byConference?season=20182019, https://statsapi.web.nhl.com/api/v1/standings/byDivision?season=20182019
-// standing_types => https://statsapi.web.nhl.com/api/v1/standingsTypes
-// team => https://statsapi.web.nhl.com/api/v1/teams/8
-// team_stats => http://statsapi.web.nhl.com/api/v1/teams/14?expand=team.stats&season=20192020
-// team_player(roster) => https://statsapi.web.nhl.com/api/v1/teams/8/roster
--- test.js
+++ test.js
@@ -34,3 +34,42 @@
 // //     document.body.append(queenQuote)
 // //   })
 // // }
+
+
+
+
+
+
+//
+
+//RESOURCES
+
+
+
+
+// https://cors-anywhere.herokuapp.com/
+
+
+// conferences => https://statsapi.web.nhl.com/api/v1/conferences
+// dates => https://statsapi.web.nhl.com/api/v1/schedule?season=20172018
+// divisions => https://statsapi.web.nhl.com/api/v1/divisions
+// franchises => https://statsapi.web.nhl.com/api/v1/franchises
+// games => https://statsapi.web.nhl.com/api/v1/schedule?season=20172018
+// game_status => https://statsapi.web.nhl.com/api/v1/gameStatus
+// game_types => https://statsapi.web.nhl.com/api/v1/gameTypes
+// game_results => https://statsapi.web.nhl.com/api/v1/playTypes
+// game_events, game_events_players => https://statsapi.web.nhl.com/api/v1/game/2017020395/feed/live
+// game_shifts => https://api.nhle.com/stats/rest/en/shiftcharts?cayenneExp=gameId=2019030212
+// player => https://statsapi.web.nhl.com/api/v1/people/8471214
+// player_regular_season_stats - https://statsapi.web.nhl.com/api/v1/people/8471214?expand=person.stats&stats=yearByYear&expand=stats.team&site=en_nhl
+// player_playoffs_stats - https://statsapi.web.nhl.com/api/v1/people/8471214?expand=person.stats&stats=yearByYearPlayoffs&expand=stats.team&site=en_nhl
+// player_game_logs - https://statsapi.web.nhl.com/api/v1/people/8471214?expand=person.stats&stats=gameLog&expand=stats.team&site=en_nhl&season=20182019
+// play_types => https://statsapi.web.nhl.com/api/v1/playTypes
+// positions - https://statsapi.web.nhl.com/api/v1/positions
+// schedules => https://statsapi.web.nhl.com/api/v1/schedule?season=20172018
+// seasons => https://statsapi.web.nhl.com/api/v1/seasons
+// standings => https://statsapi.web.nhl.com/api/v1/standings/byLeague?season=20182019 , https://statsapi.web.nhl.com/api/v1/standings/byConference?season=20182019, https://statsapi.web.nhl.com/api/v1/standings/byDivision?season=20182019
+// standing_types => https://statsapi.web.nhl.com/api/v1/standingsTypes
+// team => https://statsapi.web.nhl.com/api/v1/teams/8
+// team_stats => http://statsapi.web.nhl.com/api/v1/teams/14?expand=team.stats&season=20192020
+// team_player(roster) => https://statsapi.web.nhl.com/api/v1/teams/8/roster
