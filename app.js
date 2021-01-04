40adca6db90ce15e0a3a49fab9a3b75598f683fe
Shayne Vaykovich <VAYKO@Shaynes-MacBook-Pro.local>
2021-01-04 15:03:56 -0500

    players

--- app.js
+++ app.js
@@ -3,6 +3,9 @@
 
 const DOMAIN = `http://statsapi.web.nhl.com/api/v1/teams`
 const teamSelectSection = document.querySelector(".team-select-section")
+const playerSelectSection = document.querySelector(".player-select-section")
+
+
 
 //tryCatch
 const getData = async () => {
@@ -10,6 +13,7 @@ const getData = async () => {
     const getInfo = document.querySelector("form").value
     const response = await axios.get(`${DOMAIN}`)
     console.log(response.data)
+    
     renderSelect(response.data.teams)
   }
   catch (error) {
@@ -28,7 +32,9 @@ function renderSelect(teams) {
     let teamOption = document.createElement('option')
     teamOption.innerHTML = `<h2>${team.name}</h2>`
     teamOption.value = team.id
+    
     select.append(teamOption)
+    
   })
   teamSelectSection.append(select)
 }
@@ -37,11 +43,13 @@ function renderSelect(teams) {
 async function getTeamData(id) {
   // console.log(id)
   try {
-    const response = await axios.get(`http://statsapi.web.nhl.com/api/v1/teams/${id}/`)
+    const response = await axios.get(`${DOMAIN}/${id}`)
     const response1 = await axios.get(`http://statsapi.web.nhl.com/api/v1/teams/${id}/roster/`)
-    console.log(response1.data.roster)
+    
+    console.log(response1.data.roster[0].person.fullName)
     renderteamData(response.data)
-    renderRoster(response.data.roster)
+    
+    renderRoster(response1.data.roster)
   }
   catch (error) {
   } 
@@ -57,24 +65,26 @@ function renderteamData(data) {
   <h2>${data.teams[0].venue.name}</h2>
   <h3>${data.teams[0].conference.name} Confernece</h3>
   <h3>EST. ${data.teams[0].firstYearOfPlay}</h3>
-  <a>${data.teams[0].officialSiteUrl}</a>
+  <a href=${data.teams[0].officialSiteUrl} target='_blank'>${data.teams[0].officialSiteUrl}</a>
+
   `
   
 }
-function playerSelect(players) {
-  const choice = document.createElement('choice')
-  choice.addEventListener("change", () => {
-    getTeamData(choice.value)
+
+function renderRoster() {
+  const select1 = document.createElement('select')
+  select1.addEventListener("change", () => {
+    console.log(select1.value)
+    getTeamData(select1.value)
   })
   players.forEach((player) => {
     let playerOption = document.createElement('option')
-    playerOption.innerHTML = `<h2>${person.name}</h2>`
-    playerOption.value = person.id
-    choice.append(teamOption)
+    playerOption.innerHTML = `<h2>${team.name}</h2>`
+    playerOption.value = team.id
+    select1.append(playerOption)
   })
-  teamSelectSection.append(select)
+  playerSelectSection.append(select1)
 }
-
 
 
   
